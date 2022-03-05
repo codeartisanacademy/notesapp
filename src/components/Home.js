c811528bc2b6fa05bb166b842e7489c2c1ce60fb
wahyudi <wahyudi.coding@gmail.com>
2022-03-05 14:16:37 +0700

    modal add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { View, Text, SafeAreaView, FlatList, StyleSheet, TouchableOpacity, Modal } from 'react-native'
+import { View, Text, SafeAreaView, FlatList, StyleSheet, TouchableOpacity, Modal, TextInput } from 'react-native'
 import { useState, useEffect } from 'react';
 import { firestoreDb } from '../firbase-config';
-import { collection, getDocs, onSnapshot } from 'firebase/firestore';
+import { collection, getDocs, onSnapshot, setDoc } from 'firebase/firestore';
+import { Ionicons } from '@expo/vector-icons'; 
 
 const Home = () => {
     const [myNotes, setMyNotes] = useState([]);
     const notesCollectionRef = collection(firestoreDb, "notes");
     const [modalDetailVisible, setModalDetailVisible] = useState(false);
+    const [modalAddNotesVisible, setModalAddNotesVisible] = useState(false);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
     const [selected, setSelected] = useState(null);
 
     const getNotes = async () => {
@@ -22,9 +26,17 @@ const Home = () => {
         setMyNotes(notes);
     }
 
-    const handleDetailPress = ()=>{
+    const handleDetailPress = (item)=>{
         setModalDetailVisible(true);
+        setSelected(item);
         
+    }
+
+    const openModalAddNotes = ()=>{
+        setModalAddNotesVisible(true);
+    }
+
+    const handleSubmit = async ()=>{
     }
 
     useEffect(() => {
@@ -37,7 +49,7 @@ const Home = () => {
         <SafeAreaView>
             <View style={styles.container}>
                 <View style={{flexDirection:'row', justifyContent:'end', marginBottom:20}}>
-                    <TouchableOpacity style={{backgroundColor:'#fff', padding:10, borderRadius:8}}><Text>+</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={openModalAddNotes}><Ionicons name="add-circle-outline" size={24} color="black" /></TouchableOpacity>
                 </View>
                 <Text style={{fontWeight:"bold", marginBottom:10}}>All Notes</Text>
                 <FlatList
@@ -45,7 +57,7 @@ const Home = () => {
                     renderItem={({item})=>{
                         return(
                             <View style={{paddingVertical:8}}>
-                                <TouchableOpacity onPress={handleDetailPress}><Text>{item.title}</Text></TouchableOpacity>
+                                <TouchableOpacity onPress={()=>(handleDetailPress(item))}><Text>{item.title}</Text></TouchableOpacity>
                             </View>
                         )
                     }}
@@ -61,9 +73,41 @@ const Home = () => {
                 }}
             >
                 <View style={{flexDirection:'row', justifyContent:"end", padding:20}}>
-                    <TouchableOpacity onPress={()=>setModalDetailVisible(false)}><Text>Close</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={()=>setModalDetailVisible(false)}><Ionicons name="close-circle-outline" size={24} color="black" /></TouchableOpacity>
+                    
                 </View>
-                
+                <View style={{padding:20, marginVertical:20}}>
+                    <Text style={{fontSize:30, fontWeight:"bold"}}>{selected?.title}</Text>
+                    <View style={{marginVertical:20}}>
+                        <Text style={{fontSize:20}}>{selected?.content}</Text>
+                    </View>
+                </View>
+            </Modal>
+
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={modalAddNotesVisible}
+                onRequestClose={() => {
+                    setModalAddNotesVisible(!modalAddNotesVisible);
+                }}
+            >
+                <View style={{flexDirection:'row', justifyContent:"end", padding:20}}>
+                    <TouchableOpacity onPress={()=>setModalAddNotesVisible(false)}><Ionicons name="close-circle-outline" size={24} color="black" /></TouchableOpacity>
+                </View>
+                <View style={{padding:20, marginVertical:20}}>
+                    <Text style={{fontSize:30, fontWeight:"bold"}}>Add New Notes</Text>
+                    <View style={{marginVertical:20}}>
+                        <Text>Title</Text>
+                        <TextInput onChangeText={(value)=>setTitle(value)} style={{borderColor:"#cecece", borderWidth:1, padding:10, marginTop:8, marginBottom:20}} />
+                        <Text>Content</Text>
+                        <TextInput multiline={true} placeholder="enter the content" style={{height:100,borderColor:"#cecece", borderWidth:1, padding:10, marginTop:8, marginBottom:20   }} onChangeText={(value)=>setContent(value)} />
+
+                        <TouchableOpacity style={{padding:10, backgroundColor:'#000', borderRadius:10}} onPress={handleSubmit} >
+                            <Text style={{color:'#ffffff', alignSelf:"center"}}>Submit</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </Modal>
             
         </SafeAreaView>
